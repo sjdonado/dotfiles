7f65f08487b4c31ac12af42d5a991c113eaa5187
Juan Rodriguez <sjdonado@uninorte.edu.co>
2023-02-11 08:26:34 +0100

    fix: toggleterm toggle in zen-mode
    - check if term buffer is open
    - fix copilot npm path

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -1,13 +1,35 @@
 local nnoremap = require("sjdonado.keymap").nnoremap
-local dapui = require("sjdonado.dap").dapui
 
 require("toggleterm").setup({
 	size = 25,
 })
 
+local function has_term_buffer()
+	local bufs = vim.api.nvim_list_bufs()
+	for _, buf in ipairs(bufs) do
+		if vim.api.nvim_buf_get_option(buf, "bufhidden") == "terminal" then
+			return true
+		end
+	end
+	return false
+end
+
+local function toggle()
+	require("dapui").close({ "all" })
+
+	if require("zen-mode.view").is_open() then
+		require("zen-mode").close()
+	end
+
+	if has_term_buffer() then
+		vim.cmd(":ToggleTermToggleAll<CR>")
+	else
+		vim.cmd(":ToggleTerm<CR>")
+	end
+end
+
 nnoremap("<leader>;", function()
-	dapui.close()
-	vim.cmd(":ToggleTerm<CR>")
+	toggle()
 end)
 
 nnoremap("<leader>1;", function()
@@ -18,16 +40,11 @@ nnoremap("<leader>2;", function()
 	vim.cmd(":2ToggleTerm<CR>")
 end)
 
-nnoremap("<leader>3;", function()
-	dapui.close()
-	vim.cmd("ToggleTermToggleAll")
-end)
-
 function _G.set_terminal_keymaps()
 	local opts = { buffer = 0 }
 	vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
 	vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+	vim.keymap.set("t", "<leader>;", toggle, opts)
 end
 
--- if you only want these mappings for toggle term use term://*toggleterm#* instead
-vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+vim.cmd("autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()")
