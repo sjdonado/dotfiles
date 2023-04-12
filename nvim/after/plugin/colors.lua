2250060b19125178be113b92a28648631cb56cca
Juan Rodriguez <sjdonado@uninorte.edu.co>
2023-04-12 21:24:38 +0200

    refactor: switch dark theme to github_dark_default

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -3,13 +3,13 @@ local githubtheme = require("github-theme")
 local lualine = require("sjdonado.lualine")
 
 githubtheme.setup({
-  dark_float = true,
+  sidebars = { "nvim-tree", "terminal", "packer", "mason" },
+  darkSidebar = true,
 })
 
 -- Hide CursorLine on leave window (ignore Floating windows)
 vim.cmd("autocmd WinEnter *.*,*.git/* setlocal cursorline")
 vim.cmd("autocmd WinLeave *.*,*.git/* setlocal nocursorline")
 
-vim.api.nvim_set_option("background", "dark")
-vim.cmd("colorscheme github_dark")
+vim.cmd("colorscheme github_dark_default")
 lualine.load()
