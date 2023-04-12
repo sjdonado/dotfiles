bb9af9a0dba2b1638c350c7f38a8aec5ec1a9dfb
Juan Rodriguez <sjdonado@uninorte.edu.co>
2023-04-12 21:25:33 +0200

    fix: keymaps exit with <C-c>

--- nvim/after/plugin/nvim-tree.lua
+++ nvim/after/plugin/nvim-tree.lua
@@ -1,33 +1,35 @@
 require("nvim-tree").setup({
-	actions = {
-		open_file = {
-			quit_on_open = true,
-		},
-	},
-	view = {
-		mappings = {
-			custom_only = true,
-			list = {
-				{ key = { "<CR>", "o", "<2-LeftMouse>" }, action = "edit" },
-				{ key = "I", action = "toggle_git_ignored" },
-				{ key = "H", action = "toggle_dotfiles" },
-				{ key = "U", action = "toggle_custom" },
-				{ key = "R", action = "refresh" },
-				{ key = "a", action = "create" },
-				{ key = "d", action = "remove" },
-				{ key = "D", action = "trash" },
-				{ key = "r", action = "rename" },
-				{ key = "x", action = "cut" },
-				{ key = "c", action = "copy" },
-				{ key = "p", action = "paste" },
-				{ key = "y", action = "copy_name" },
-				{ key = "Y", action = "copy_path" },
-				{ key = "gy", action = "copy_absolute_path" },
-				{ key = "W", action = "collapse_all" },
-				{ key = "E", action = "expand_all" },
-			},
-		},
-	},
+  actions = {
+    open_file = {
+      quit_on_open = true,
+    },
+  },
+  view = {
+    mappings = {
+      custom_only = true,
+      list = {
+        { key = { "<CR>", "o", "<2-LeftMouse>" }, action = "edit" },
+        { key = "I", action = "toggle_git_ignored" },
+        { key = "H", action = "toggle_dotfiles" },
+        { key = "U", action = "toggle_custom" },
+        { key = "R", action = "refresh" },
+        { key = "a", action = "create" },
+        { key = "d", action = "remove" },
+        { key = "D", action = "trash" },
+        { key = "r", action = "rename" },
+        { key = "x", action = "cut" },
+        { key = "c", action = "copy" },
+        { key = "p", action = "paste" },
+        { key = "y", action = "copy_name" },
+        { key = "Y", action = "copy_path" },
+        { key = "gy", action = "copy_absolute_path" },
+        { key = "W", action = "collapse_all" },
+        { key = "E", action = "expand_all" },
+        { key = "<C-c>", action = "close" },
+        { key = "g?", action = "toggle_help" },
+      },
+    },
+  },
 })
 
 -- nvim tree config
