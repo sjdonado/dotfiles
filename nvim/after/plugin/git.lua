94f3c8e93455b17e88dad8f544d47293527b5bf5
Juan Rodriguez <sjdonado@uninorte.edu.co>
2023-04-13 12:31:12 +0200

    fix: keymaps + minor typo

--- nvim/after/plugin/git.lua
+++ nvim/after/plugin/git.lua
@@ -16,20 +16,13 @@ gitsigns.setup({
     -- Actions
     map({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>")
     map({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk<CR>")
-    map("n", "<leader>hS", gs.stage_buffer)
     map("n", "<leader>hu", gs.undo_stage_hunk)
+    map("n", "<leader>hS", gs.stage_buffer)
     map("n", "<leader>hR", gs.reset_buffer)
     map("n", "<leader>hp", gs.preview_hunk)
     map("n", "<leader>hb", function()
       gs.blame_line({ full = true })
     end)
-    map("n", "<leader>hd", gs.diffthis)
-    map("n", "<leader>hD", function()
-      gs.diffthis("~")
-    end)
-
-    -- Text object
-    map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
   end,
 })
 
