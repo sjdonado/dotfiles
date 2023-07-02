415f1562d35e23ef5f7891d8b9cfe244c9148831
Juan Rodriguez <sjdonado@uninorte.edu.co>
2023-07-02 14:32:15 +0200

    fix: nvim-tree mappings on_attach callback

--- nvim/after/plugin/nvim-tree.lua
+++ nvim/after/plugin/nvim-tree.lua
@@ -6,31 +6,42 @@ require("nvim-tree").setup({
   },
   view = {
     width = 60,
-    mappings = {
-      custom_only = true,
-      list = {
-        { key = { "<CR>", "o", "<2-LeftMouse>" }, action = "edit" },
-        { key = "I", action = "toggle_git_ignored" },
-        { key = "H", action = "toggle_dotfiles" },
-        { key = "U", action = "toggle_custom" },
-        { key = "R", action = "refresh" },
-        { key = "a", action = "create" },
-        { key = "d", action = "remove" },
-        { key = "D", action = "trash" },
-        { key = "r", action = "rename" },
-        { key = "x", action = "cut" },
-        { key = "c", action = "copy" },
-        { key = "p", action = "paste" },
-        { key = "y", action = "copy_name" },
-        { key = "Y", action = "copy_path" },
-        { key = "gy", action = "copy_absolute_path" },
-        { key = "W", action = "collapse_all" },
-        { key = "E", action = "expand_all" },
-        { key = "q", action = "close" },
-        { key = "g?", action = "toggle_help" },
-      },
-    },
   },
+  on_attach = function(bufnr)
+    local api = require("nvim-tree.api")
+
+    local function opts(desc)
+      return {
+        desc = "nvim-tree: " .. desc,
+        buffer = bufnr,
+        noremap = true,
+        silent = true,
+        nowait = true,
+      }
+    end
+
+    vim.keymap.set("n", "<CR>", api.node.open.edit, opts("Open"))
+    vim.keymap.set("n", "o", api.node.open.edit, opts("Open"))
+    vim.keymap.set("n", "<2-LeftMouse>", api.node.open.edit, opts("Open"))
+    vim.keymap.set("n", "I", api.tree.toggle_gitignore_filter, opts("Toggle Git Ignore"))
+    vim.keymap.set("n", "H", api.tree.toggle_hidden_filter, opts("Toggle Dotfiles"))
+    vim.keymap.set("n", "U", api.tree.toggle_custom_filter, opts("Toggle Hidden"))
+    vim.keymap.set("n", "R", api.tree.reload, opts("Refresh"))
+    vim.keymap.set("n", "a", api.fs.create, opts("Create"))
+    vim.keymap.set("n", "d", api.fs.remove, opts("Delete"))
+    vim.keymap.set("n", "D", api.fs.trash, opts("Trash"))
+    vim.keymap.set("n", "r", api.fs.rename, opts("Rename"))
+    vim.keymap.set("n", "x", api.fs.cut, opts("Cut"))
+    vim.keymap.set("n", "c", api.fs.copy.node, opts("Copy"))
+    vim.keymap.set("n", "p", api.fs.paste, opts("Paste"))
+    vim.keymap.set("n", "y", api.fs.copy.filename, opts("Copy Name"))
+    vim.keymap.set("n", "Y", api.fs.copy.relative_path, opts("Copy Relative Path"))
+    vim.keymap.set("n", "gy", api.fs.copy.absolute_path, opts("Copy Absolute Path"))
+    vim.keymap.set("n", "W", api.tree.collapse_all, opts("Collapse"))
+    vim.keymap.set("n", "E", api.tree.expand_all, opts("Expand All"))
+    vim.keymap.set("n", "q", api.tree.close, opts("Close"))
+    vim.keymap.set("n", "g?", api.tree.toggle_help, opts("Help"))
+  end,
 })
 
 -- nvim tree config
