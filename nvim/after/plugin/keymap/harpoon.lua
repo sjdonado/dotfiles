7181aae1061d9d5608608bc7be22bf1f12679655
Juan Rodriguez <sjdonado@uninorte.edu.co>
2023-09-26 07:01:03 -0500

    feat: harpoon nav file keymaps

--- nvim/after/plugin/keymap/harpoon.lua
+++ nvim/after/plugin/keymap/harpoon.lua
@@ -1,9 +1,29 @@
-local map = require("sjdonado.keymap").map
+local nnoremap = require("sjdonado.keymap").nnoremap
 
-map({ "n", "v" }, "<leader>a", function()
+nnoremap("<leader>a", function()
   require("harpoon.mark").add_file()
 end, { silent = true })
 
-map({ "n", "v" }, "<leader><Tab>", function()
+nnoremap("<leader><Tab>", function()
   require("harpoon.ui").toggle_quick_menu()
+end, { silent = true })
+
+nnoremap("<leader>1", function()
+  require("harpoon.ui").nav_file(1)
+end, { silent = true })
+
+nnoremap("<leader>2", function()
+  require("harpoon.ui").nav_file(2)
+end, { silent = true })
+
+nnoremap("<leader>3", function()
+  require("harpoon.ui").nav_file(3)
+end, { silent = true })
+
+nnoremap("<leader>4", function()
+  require("harpoon.ui").nav_file(4)
+end, { silent = true })
+
+nnoremap("<leader>5", function()
+  require("harpoon.ui").nav_file(5)
 end, { silent = true })
