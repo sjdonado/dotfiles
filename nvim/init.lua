5d98bad83742ecaa184b19f34b7852566f9be5c5
Juan Rodriguez <sjdonado@icloud.com>
2024-04-19 11:05:03 +0200

    feat: kickstart - lint + lsp setup

--- nvim/init.lua
+++ nvim/init.lua
@@ -472,18 +472,24 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
+        clangd = {},
+        gopls = {},
         -- pyright = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
-        --
+        tsserver = {
+          settings = {
+            codeAction = {
+              MoveToANewFile = { enable = false },
+            },
+          },
+        },
+
+        jsonls = {},
 
         lua_ls = {
           -- cmd = {...},
@@ -513,7 +519,10 @@ require('lazy').setup({
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
+        'stylua',
+        'eslint_d',
+        'prettierd',
+        'vale',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
