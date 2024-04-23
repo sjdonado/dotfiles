e5bc6baadef877a7c695b651c9f937fbbcf3a79e
Juan Rodriguez <sjdonado@icloud.com>
2024-04-23 10:31:57 +0200

    fix: missing cmp-buffer source

--- nvim/init.lua
+++ nvim/init.lua
@@ -512,10 +512,8 @@ require('lazy').setup({
       },
       'saadparwaiz1/cmp_luasnip',
 
-      -- Adds other completion capabilities.
-      --  nvim-cmp does not ship with all sources by default. They are split
-      --  into multiple repos for maintenance purposes.
       'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-path',
     },
     config = function()
@@ -585,8 +583,8 @@ require('lazy').setup({
         sources = {
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
-          { name = 'path' },
           { name = 'buffer' },
+          { name = 'path' },
         },
       }
     end,
