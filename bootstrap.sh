9a760b96e79f8f05256240b7e38cf88582a5e702
Juan Rodriguez <sjdonado@icloud.com>
2024-05-03 03:46:52 +0200

    chore: update boostrap.sh macos installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,18 +4,28 @@
 ##            macos setup               ##
 ##########################################
 
+# Best font ever
+brew tap homebrew/cask-fonts
+brew install font-hack-nerd-font
+
 # Setup alacritty
 brew install alacritty
-ln -s "$PWD/alacritty/alacritty.toml" ~/.config/alacritty.toml
+ln -sf "$PWD/alacritty/alacritty.toml" ~/.config/alacritty.toml
 
 # Tmux config
 brew install tmux
+
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+ln -sn "$PWD/tmux/.tmux.conf" ~/.tmux.conf
 
 # Fish shell
 brew install fish
+
 echo "/opt/homebrew/bin/fish" | sudo tee -a /etc/shells
-ln -sf "$PWD/fish/config.fish" ~/.config/fish/config.fish
+chsh -s /opt/homebrew/bin/fish
+
+mkdir -p ~/.config/fish/config.fish && \
+ln -s "$PWD/fish/config.fish" ~/.config/fish/config.fish
 
 # Nvim config
 brew install nvim lua
@@ -25,31 +35,33 @@ ln -s "$PWD/nvim" ~/.config/nvim
 brew install coreutils \
   tree bat fd gawk ripgrep \
   blueutil mackup \
-  git-delta git-lfs \
-  tor mitmproxy nmap redis \
-  cloudflare/cloudflare/cloudflared \
-  marp-cli mailpit bruno \
+  git-delta git-lfs
+
+brew install --cask \
+  firefox \
+  raycast ukelele notunes browserosaurus \
+
+brew install --cask --no-quarantine chromium
+
+# Tiling Window manager
+brew install --cask amethyst # SIP unblocked not required
+
+# Package managers
+brew install fnm rustup luarocks pypenv # run rustup-init
+ln -sf "$PWD/fish/conf.d/fnm.fish" ~/.config/fish/conf.d/fnm.fish
 
 # Docker
 brew install colima docker docker-compose docker-buildx
 colima start
 
-# Package managers
-brew install fnm rustup luarocks pypenv
-ln -sf "$PWD/fish/conf.d/fnm.fish" ~/.config/fish/conf.d/fnm.fish
-
-# Tiling Window manager
-brew install --cask amethyst # SIP unblocked not required
-
-# Casks
+# Nice to have
 brew install --cask \
-  firefox \
-  raycast ukelele \
   table-tool db-browser-for-sqlite dbeaver-community \
-  qbittorrent reverso utm browserosaurus \
-  notunes
+  qbittorrent reverso utm
 
-brew install --cask --no-quarantine chromium
+brew install tor mitmproxy nmap redis \
+  cloudflare/cloudflare/cloudflared \
+  marp-cli mailpit
 
 # Toolkit
 brew tap shopify/shopify
@@ -58,19 +70,11 @@ brew install shopify-cli firebase-cli scc serverless
 brew tap oven-sh/bun
 brew install bun
 
-# Best font ever
-brew tap homebrew/cask-fonts
-brew install font-hack-nerd-font
-
 # Keyboard layouts setup
-cp -Rp $PWD/ukelele/* "$HOME/Library/Keyboard Layouts/"
+cp -Rp $PWD/macos/ukelele/* "$HOME/Library/Keyboard Layouts/"
 
 # Dotfiles symlinks
 ln -sf "$PWD/bat/config" ~/.config/bat/config
-
-ln -s "$PWD/tmux/.tmux.conf" ~/.tmux.conf
-
-ln -s "$PWD/git/.gitconfig" ~/.gitconfig
+ln -sf "$PWD/git/.gitconfig" ~/.gitconfig
 ln -s "$PWD/.ssh/config" ~/.ssh/config
-
 ln -s "$PWD/.mackup.cfg" ~/.mackup.cfg
