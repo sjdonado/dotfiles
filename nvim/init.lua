15adae18bcd81b33d70b19952d0ac7692a93ae27
Juan Rodriguez <sjdonado@icloud.com>
2024-09-10 10:28:30 +0200

    fix: replace none-ls with conform

--- nvim/init.lua
+++ nvim/init.lua
@@ -376,12 +376,11 @@ require('lazy').setup({
       capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
       local servers = {
+        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         clangd = {},
         pyright = {},
         rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --    TODO: https://github.com/pmizio/typescript-tools.nvim (check when beta is done)
-        tsserver = {},
+        ts_ls = {},
 
         eslint = {
           on_attach = function(client, bufnr)
@@ -470,37 +469,47 @@ require('lazy').setup({
     end,
   },
 
-  {
-    'nvimtools/none-ls.nvim',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-    },
-    config = function()
-      local null_ls = require 'null-ls'
-
-      local sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.isort,
-        null_ls.builtins.formatting.prettier.with {
-          extra_filetypes = { 'javascript', 'typescript', 'javascriptreact', 'typescriptreact' },
-        },
-      }
-
-      null_ls.setup {
-        sources = sources,
-        on_attach = function(client, bufnr)
-          if client.server_capabilities.documentFormattingProvider then
-            vim.api.nvim_create_autocmd('BufWritePre', {
-              buffer = bufnr,
-              callback = function()
-                vim.cmd 'silent! lua vim.lsp.buf.format { bufnr = bufnr }'
-              end,
-            })
-          end
+  { -- Autoformat
+    'stevearc/conform.nvim',
+    event = { 'BufWritePre' },
+    cmd = { 'ConformInfo' },
+    keys = {
+      {
+        '<leader>f',
+        function()
+          require('conform').format { async = true, lsp_format = 'fallback' }
         end,
-      }
-    end,
+        mode = '',
+        desc = '[F]ormat buffer',
+      },
+    },
+    opts = {
+      notify_on_error = false,
+      format_on_save = function(bufnr)
+        -- Disable "format_on_save lsp_fallback" for languages that don't
+        -- have a well standardized coding style. You can add additional
+        -- languages here or re-enable it for the disabled ones.
+        local disable_filetypes = { c = true, cpp = true }
+        local lsp_format_opt
+        if disable_filetypes[vim.bo[bufnr].filetype] then
+          lsp_format_opt = 'never'
+        else
+          lsp_format_opt = 'fallback'
+        end
+        return {
+          timeout_ms = 500,
+          lsp_format = lsp_format_opt,
+        }
+      end,
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        python = { 'isort', 'black' },
+        javascript = { 'prettier' },
+        javascriptreact = { 'prettier' },
+        typescript = { 'prettier' },
+        typescriptreact = { 'prettier' },
+      },
+    },
   },
 
   { -- Autocompletion
