182832a02bb523b865f052355a17aac5ce02f2aa
Juan Rodriguez <sjdonado@icloud.com>
2024-10-13 14:30:36 +0200

    refactor: simplify bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,12 +7,7 @@
 # Install dependencies from Brewfile
 brew bundle install
 
-# Setup fonts
-# brew tap homebrew/cask-fonts
-# brew install font-hack-nerd-font
-
-# # Setup alacritty
-# brew install alacritty
+# Setup alacritty
 ln -sf "$PWD/alacritty/alacritty.toml" ~/.config/alacritty.toml
 
 # # Fix alacritty thin strokes on macos
@@ -23,70 +18,28 @@ mkdir -p ~/.config/alacritty/themes
 ln -sf $PWD/alacritty/themes/light.toml ~/.config/alacritty/themes/light.toml
 ln -sf $PWD/alacritty/themes/dark.toml ~/.config/alacritty/themes/dark.toml
 
-# Setup kitty
-# brew install kitty
+# Setup kitty (optional)
 # ln -sf $PWD/kitty/kitty.conf ~/.config/kitty/kitty.conf
 # ln -sf $PWD/kitty/light-theme.conf ~/.config/kitty/light-theme.conf
 # ln -sf $PWD/kitty/dark-theme.conf ~/.config/kitty/dark-theme.conf
 
 # Tmux config
-# brew install tmux
-
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -sn "$PWD/tmux/.tmux.conf" ~/.tmux.conf
 
-# Shell setup
-# brew install fzf
-
+# Shell theme setup
 chsh -s /bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 ln -sf "$PWD/zsh/.zshrc" ~/.zshrc
 ln -sf "$PWD/zsh/sjdonado.zsh-theme" ~/.oh-my-zsh/themes/sjdonado.zsh-theme
 
-# Nvim config
-# brew install nvim lua
-ln -s "$PWD/nvim" ~/.config/nvim
-
-# # Essentials
-# brew install coreutils \
-#   tree bat fd gawk ripgrep \
-#   blueutil mackup \
-#   git-delta git-lfs
-#
-# brew install --cask \
-#   firefox hoppscotch \
-#   spotify libreoffice \
-#   raycast ukelele notunes browserosaurus \
-#   monitorcontrol
-
 # Package managers
-# brew install fnm rustup luarocks pypenv crystal
 rustup-init
 
 # Docker
-# brew install colima docker docker-compose docker-buildx
 colima start
-
 echo '"cliPluginsExtraDirs": ["/opt/homebrew/lib/docker/cli-plugins"]' >> ~/.docker/config.json
-
-# Nice to have
-brew install --cask \
-  tableplus \
-  qbittorrent netnewswire wealthfolio
-
-# Tailscale setup
-# brew install tailscale mosh
-# sudo tailscaled install-system-daemon
-# tailscale up
-
-# brew install tor mitmproxy nmap redis \
-#   cloudflare/cloudflare/cloudflared \
-#   mailpit
-
-# Toolkit
-brew tap shopify/shopify
-brew install shopify-cli firebase-cli scc serverless
 
 brew tap oven-sh/bun
 brew install bun
@@ -102,3 +55,6 @@ ln -sf "$PWD/bat/config" ~/.config/bat/config
 ln -sf "$PWD/git/.gitconfig" ~/.gitconfig
 ln -s "$PWD/.ssh/config" ~/.ssh/config
 ln -s "$PWD/.mackup.cfg" ~/.mackup.cfg
+
+# Nvim config
+ln -s "$PWD/nvim" ~/.config/nvim
