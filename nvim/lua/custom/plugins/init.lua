4d31f91f23de1c97f5a530a8032b5392e5b15453
sjdonado <sjdonado@icloud.com>
2025-02-26 19:45:14 +0100

    feat: replace neogit with vim fugitive

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -1,87 +1,95 @@
 return {
   { -- Smooth scroll
-    'karb94/neoscroll.nvim',
+    "karb94/neoscroll.nvim",
     opts = {
       mappings = { -- Keys to be mapped to their corresponding default scrolling animation
-        '<C-u>',
-        '<C-d>',
-        '<C-b>',
-        '<C-f>',
-        '<C-y>',
-        '<C-e>',
-        'zt',
-        'zz',
-        'zb',
+        "<C-u>",
+        "<C-d>",
+        "<C-b>",
+        "<C-f>",
+        "<C-y>",
+        "<C-e>",
+        "zt",
+        "zz",
+        "zb",
       },
     },
   },
-  -- { -- Color scheme
-  --   'Mofiqul/vscode.nvim',
-  --   config = function()
-  --     local c = require('vscode.colors').get_colors()
-  --     require('vscode').setup {
-  --       -- style = 'light',
-  --       disable_nvimtree_bg = true,
-  --       group_overrides = {
-  --         TelescopePromptBorder = { fg = c.vscCursorDark, bg = 'NONE' },
-  --         TelescopeResultsBorder = { fg = c.vscCursorDark, bg = 'NONE' },
-  --         TelescopePreviewBorder = { fg = c.vscCursorDark, bg = 'NONE' },
-  --         DiagnosticHint = { fg = c.vscGray, bg = 'NONE' },
-  --         DiagnosticUnderlineHint = { fg = 'NONE', bg = 'NONE', undercurl = true, sp = c.vscGray },
-  --       },
-  --     }
-  --     vim.cmd.colorscheme 'vscode'
-  --   end,
-  -- },
   {
-    'rmagatti/auto-session',
+    "rmagatti/auto-session",
     lazy = false,
     dependencies = {
-      'nvim-telescope/telescope.nvim',
+      "nvim-telescope/telescope.nvim",
     },
     opts = {
-      auto_session_suppress_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+      auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
     },
   },
-  { 'akinsho/git-conflict.nvim', version = '*', config = true },
   {
-    'sindrets/diffview.nvim',
-    lazy = false,
+    "tpope/vim-fugitive",
     keys = {
-      { '<leader>gh', '<cmd>DiffviewFileHistory %<CR>', desc = 'File Diff View History' },
-      { '<leader>gm', '<cmd>DiffviewOpen<CR>', desc = 'Merge Tool (Single Panel)' },
-      { '<leader>gt', '<cmd>DiffviewToggle<CR>', desc = 'Toggle Diff View' },
-      { '<leader>gc', '<cmd>DiffviewClose<CR>', desc = 'Close Diff View' },
+      { "<leader>gs", "<cmd>tab Git<CR>", desc = "Git Status" },
+      { "<leader>gb", "<cmd>Git blame<CR>", desc = "Git Blame" },
+      { "<leader>gd", "<cmd>tab Git diff<CR>", desc = "Git Diff" },
+      { "<leader>gl", "<cmd>tab Git log<CR>", desc = "Git Log" },
+      { "<leader>gp", "<cmd>Git push<CR>", desc = "Git Push" },
+      { "<leader>gP", "<cmd>Git pull<CR>", desc = "Git Pull" },
+      { "<leader>gc", "<cmd>tab Git commit<CR>", desc = "Git Commit" },
+      { "<leader>gC", "<cmd>Git commit --amend<CR>", desc = "Git Commit Amend" },
+      { "<leader>ga", "<cmd>Git add %<CR>", desc = "Git Add Current File" },
+      { "<leader>gA", "<cmd>Git add .<CR>", desc = "Git Add All" },
     },
   },
+  { "akinsho/git-conflict.nvim", version = "*", config = true },
   {
-    'NeogitOrg/neogit',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-    },
+    "sindrets/diffview.nvim",
+    lazy = false,
     opts = {
+      icons = {
+        folder_closed = "c",
+        folder_open = "o",
+      },
       signs = {
-        item = { '', '' },
-      },
-      integrations = {
-        telescope = nil,
-      },
-      mappings = {
-        status = {
-          [']x'] = 'GoToPreviousHunkHeader',
-          ['[x'] = 'GoToNextHunkHeader',
-        },
-      },
-      commit_editor = {
-        staged_diff_split_kind = 'vsplit_left',
+        fold_closed = "+",
+        fold_open = "-",
+        done = "x",
       },
     },
     keys = {
-      { '<C-g>', '<cmd>Neogit<CR>', desc = 'Open Neogit' },
+      { "<C-g>", "<cmd>DiffviewOpen<CR>", desc = "Open DiffView" },
+      { "<leader>gh", "<cmd>DiffviewFileHistory %<CR>", desc = "File Diff View History" },
     },
+    config = function(_, opts)
+      local diffview = require("diffview")
+      diffview.setup(opts)
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "DiffviewFiles", "DiffviewFileHistory" },
+        callback = function(event)
+          vim.keymap.set(
+            "n",
+            "cc",
+            "<cmd>DiffviewToggleFiles<CR><cmd>vertical Git commit<CR>",
+            { buffer = event.buf, desc = "Toggle files and commit" }
+          )
+          vim.keymap.set(
+            "n",
+            "<Esc>",
+            "<cmd>tabclose<CR>",
+            { buffer = event.buf, desc = "Close tab" }
+          )
+          vim.keymap.set(
+            "n",
+            "q",
+            "<cmd>DiffviewClose<CR>",
+            { buffer = event.buf, desc = "Close Diff View" }
+          )
+        end,
+      })
+    end,
   },
   {
-    'folke/zen-mode.nvim',
+    "folke/zen-mode.nvim",
     opts = {
       window = {
         width = 1,
@@ -94,15 +102,15 @@ return {
       end,
     },
     keys = {
-      { '<leader>z', '<cmd>ZenMode<CR>', desc = 'Toggle [Z]en Mode' },
+      { "<leader>z", "<cmd>ZenMode<CR>", desc = "Toggle [Z]en Mode" },
     },
   },
   {
-    'nvim-tree/nvim-tree.lua',
-    version = '*',
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
     lazy = false,
     dependencies = {
-      'nvim-tree/nvim-web-devicons',
+      "nvim-tree/nvim-web-devicons",
     },
     opts = {
       renderer = {
@@ -113,23 +121,23 @@ return {
           },
           glyphs = {
             folder = {
-              arrow_closed = '>',
-              arrow_open = 'v',
-              default = 'd',
-              open = 'o',
-              empty = 'e',
-              empty_open = 'eo',
-              symlink = 'l',
-              symlink_open = 'lo',
+              arrow_closed = ">",
+              arrow_open = "v",
+              default = "d",
+              open = "o",
+              empty = "e",
+              empty_open = "eo",
+              symlink = "l",
+              symlink_open = "lo",
             },
             git = {
-              unstaged = 'x',
-              staged = '+',
-              unmerged = '!',
-              renamed = 'r',
-              untracked = '?',
-              deleted = 'd',
-              ignored = 'i',
+              unstaged = "x",
+              staged = "+",
+              unmerged = "!",
+              renamed = "r",
+              untracked = "?",
+              deleted = "d",
+              ignored = "i",
             },
           },
         },
@@ -148,11 +156,11 @@ return {
         threshold = vim.log.levels.ERROR,
       },
       on_attach = function(bufnr)
-        local api = require 'nvim-tree.api'
+        local api = require("nvim-tree.api")
 
         local function opts(desc)
           return {
-            desc = 'Nvim Tree: ' .. desc,
+            desc = "Nvim Tree: " .. desc,
             buffer = bufnr,
             noremap = true,
             silent = true,
@@ -160,95 +168,95 @@ return {
           }
         end
 
-        vim.keymap.set('n', '<CR>', api.node.open.edit, opts 'Open')
-        vim.keymap.set('n', 'o', api.node.open.edit, opts 'Open')
-        vim.keymap.set('n', 'I', api.tree.toggle_gitignore_filter, opts 'Toggle Git Ignore')
-        vim.keymap.set('n', 'H', api.tree.toggle_hidden_filter, opts 'Toggle Dotfiles')
-        vim.keymap.set('n', 'U', api.tree.toggle_custom_filter, opts 'Toggle Hidden')
-        vim.keymap.set('n', 'R', api.tree.reload, opts 'Refresh')
-        vim.keymap.set('n', 'a', api.fs.create, opts 'Create')
-        vim.keymap.set('n', 'd', api.fs.remove, opts 'Delete')
-        vim.keymap.set('n', 'D', api.fs.trash, opts 'Trash')
-        vim.keymap.set('n', 'r', api.fs.rename, opts 'Rename')
-        vim.keymap.set('n', 'x', api.fs.cut, opts 'Cut')
-        vim.keymap.set('n', 'c', api.fs.copy.node, opts 'Copy')
-        vim.keymap.set('n', 'p', api.fs.paste, opts 'Paste')
-        vim.keymap.set('n', 'y', api.fs.copy.filename, opts 'Copy Name')
-        vim.keymap.set('n', 'Y', api.fs.copy.relative_path, opts 'Copy Relative Path')
-        vim.keymap.set('n', 'gy', api.fs.copy.absolute_path, opts 'Copy Absolute Path')
-        vim.keymap.set('n', 'W', api.tree.collapse_all, opts 'Collapse')
-        vim.keymap.set('n', 'E', api.tree.expand_all, opts 'Expand All')
-        vim.keymap.set('n', 'q', api.tree.close, opts 'Close')
-        vim.keymap.set('n', 'g?', api.tree.toggle_help, opts 'Help')
+        vim.keymap.set("n", "<CR>", api.node.open.edit, opts("Open"))
+        vim.keymap.set("n", "o", api.node.open.edit, opts("Open"))
+        vim.keymap.set("n", "I", api.tree.toggle_gitignore_filter, opts("Toggle Git Ignore"))
+        vim.keymap.set("n", "H", api.tree.toggle_hidden_filter, opts("Toggle Dotfiles"))
+        vim.keymap.set("n", "U", api.tree.toggle_custom_filter, opts("Toggle Hidden"))
+        vim.keymap.set("n", "R", api.tree.reload, opts("Refresh"))
+        vim.keymap.set("n", "a", api.fs.create, opts("Create"))
+        vim.keymap.set("n", "d", api.fs.remove, opts("Delete"))
+        vim.keymap.set("n", "D", api.fs.trash, opts("Trash"))
+        vim.keymap.set("n", "r", api.fs.rename, opts("Rename"))
+        vim.keymap.set("n", "x", api.fs.cut, opts("Cut"))
+        vim.keymap.set("n", "c", api.fs.copy.node, opts("Copy"))
+        vim.keymap.set("n", "p", api.fs.paste, opts("Paste"))
+        vim.keymap.set("n", "y", api.fs.copy.filename, opts("Copy Name"))
+        vim.keymap.set("n", "Y", api.fs.copy.relative_path, opts("Copy Relative Path"))
+        vim.keymap.set("n", "gy", api.fs.copy.absolute_path, opts("Copy Absolute Path"))
+        vim.keymap.set("n", "W", api.tree.collapse_all, opts("Collapse"))
+        vim.keymap.set("n", "E", api.tree.expand_all, opts("Expand All"))
+        vim.keymap.set("n", "q", api.tree.close, opts("Close"))
+        vim.keymap.set("n", "g?", api.tree.toggle_help, opts("Help"))
       end,
     },
     keys = {
-      { '<leader>e', '<cmd>NvimTreeFindFileToggle<CR>', { desc = 'Toggle Nvim Tree' } },
+      { "<leader>e", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle Nvim Tree" } },
     },
   },
   {
-    'justinmk/vim-sneak',
+    "justinmk/vim-sneak",
     keys = {
-      { 'f', '<Plug>Sneak_s', desc = 'Jump to any location specified by two charakters' },
-      { 'F', '<Plug>Sneak_S', desc = 'Jump to any location specified by two characters (reverse)' },
+      { "f", "<Plug>Sneak_s", desc = "Jump to any location specified by two charakters" },
+      { "F", "<Plug>Sneak_S", desc = "Jump to any location specified by two characters (reverse)" },
     },
   },
-  { 'wsdjeg/vim-fetch' },
-  { 'vim-crystal/vim-crystal' },
-  { 'amadeus/vim-mjml' },
+  { "wsdjeg/vim-fetch" },
+  { "vim-crystal/vim-crystal" },
+  { "amadeus/vim-mjml" },
   {
-    'norcalli/nvim-colorizer.lua',
+    "norcalli/nvim-colorizer.lua",
     opts = {
-      '*',
-      '!vim',
+      "*",
+      "!vim",
     },
   },
   {
-    'nvim-neotest/neotest',
+    "nvim-neotest/neotest",
     dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-neotest/nvim-nio',
-      'nvim-treesitter/nvim-treesitter',
-      'marilari88/neotest-vitest',
+      "nvim-lua/plenary.nvim",
+      "nvim-neotest/nvim-nio",
+      "nvim-treesitter/nvim-treesitter",
+      "marilari88/neotest-vitest",
     },
     config = function()
-      local neotest = require 'neotest'
+      local neotest = require("neotest")
 
-      neotest.setup {
+      neotest.setup({
         adapters = {
-          require 'neotest-vitest' {},
+          require("neotest-vitest")({}),
         },
         output_panel = {
           enabled = true,
         },
-      }
+      })
 
-      vim.keymap.set('n', '<leader>tn', function()
+      vim.keymap.set("n", "<leader>tn", function()
         neotest.run.run()
-      end, { desc = 'Run nearest test' })
+      end, { desc = "Run nearest test" })
 
-      vim.keymap.set('n', '<leader>tf', function()
-        neotest.run.run(vim.fn.expand '%')
-      end, { desc = 'Run tests in file' })
+      vim.keymap.set("n", "<leader>tf", function()
+        neotest.run.run(vim.fn.expand("%"))
+      end, { desc = "Run tests in file" })
 
-      vim.keymap.set('n', '<leader>to', function()
+      vim.keymap.set("n", "<leader>to", function()
         neotest.output.open()
-      end, { desc = 'Toggle output panel' })
+      end, { desc = "Toggle output panel" })
     end,
   },
   {
-    'p-nerd/sr.nvim',
+    "p-nerd/sr.nvim",
     dependencies = {
-      'nvim-telescope/telescope.nvim',
+      "nvim-telescope/telescope.nvim",
     },
     config = function()
-      require('sr').setup {
-        keymap = '<leader>s/',
+      require("sr").setup({
+        keymap = "<leader>s/",
         ignore_case = false,
         use_regex = false,
         preview_changes = true,
         live_preview = true,
-      }
+      })
     end,
   },
   -- {
