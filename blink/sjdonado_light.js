80d09198ed9096e1ab63f1bc1e98f9f150b46c7c
sjdonado <sjdonado@icloud.com>
2025-04-02 21:00:41 +0200

    feat: blink dark and light themes

---
+++ blink/sjdonado_light.js
@@ -0,0 +1,23 @@
+t.prefs_.set("color-palette-overrides", [
+  "#3a3f45",
+  "#e03b3b",
+  "#138a41",
+  "#d39d00",
+  "#1a73e8",
+  "#9b4ef9",
+  "#1ca5a5",
+  "#87939f",
+  "#4a515a",
+  "#d12038",
+  "#20a063",
+  "#c88801",
+  "#3498ff",
+  "#a56fff",
+  "#26bfbf",
+  "#adb5bd",
+]);
+t.prefs_.set("foreground-color", "#000000");
+t.prefs_.set("background-color", "#fffeff");
+t.prefs_.set("cursor-color", "#000000");
+t.prefs_.set("selection-color", "#d0e7ff");
+t.prefs_.set("selection-text-color", "#1e1e1e");
