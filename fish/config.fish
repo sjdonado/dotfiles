30b938cfb94d40155ec560a8f5573673bbe9479a
sjdonado <sjdonado@icloud.com>
2025-06-03 12:03:17 +0200

    feat: tmux kill-session keymap

--- fish/config.fish
+++ fish/config.fish
@@ -26,6 +26,7 @@ set -x PATH $HOME/.cargo/bin $PATH
 
 fish_add_path "$HOME/.local/bin"
 fish_add_path /opt/homebrew/bin
+
 fish_add_path "$HOME/.bun/bin"
 fish_add_path "$PNPM_HOME"
 
--- nvim/init.lua
+++ nvim/init.lua
@@ -562,15 +562,15 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        if disable_filetypes[vim.bo[bufnr].filetype] then
-          return nil
-        else
-          return {
-            timeout_ms = 500,
-            lsp_format = 'fallback',
-          }
-        end
+        -- local disable_filetypes = { c = true, cpp = true }
+        -- if disable_filetypes[vim.bo[bufnr].filetype] then
+        --   return nil
+        -- else
+        --   return {
+        --     timeout_ms = 500,
+        --     lsp_format = 'fallback',
+        --   }
+        -- end
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
