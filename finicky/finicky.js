ca4d364fd5552805dcebfeae12018b3b8b546e30
Juan Rodriguez Donado <sjdonado@icloud.com>
2025-09-09 12:24:28 +0200

    chore: update finicky and gitconfig

--- finicky/finicky.js
+++ finicky/finicky.js
@@ -5,5 +5,13 @@ export default {
       match: ["localhost*"],
       browser: "Chromium",
     },
+    {
+      match: ["*autarc.energy"],
+      browser: "Chromium",
+    },
+    {
+      match: ["*fly.dev"],
+      browser: "Chromium",
+    },
   ],
 };
