95da2f0b151b01ba09ee30f59ff6d06a678c0153
sjdonado <sjdonado@icloud.com>
2025-11-13 22:22:51 +0100

    chore: update martillo lua newest api

--- martillo/init.lua
+++ martillo/init.lua
@@ -1,4 +1,3 @@
--- Load Martillo
 package.path = package.path .. ";" .. os.getenv("HOME") .. "/.martillo/?.lua"
 
 return require("martillo").setup({
@@ -6,67 +5,46 @@ return require("martillo").setup({
 
 	{
 		"ActionsLauncher",
-		opts = function()
-			local window = require("bundle.window_management")
-			local utilities = require("bundle.utilities")
-			local converter = require("bundle.converter")
-			local screen = require("bundle.screen")
-			local network = require("bundle.network")
-			local clipboard = require("bundle.clipboard_history")
-			local kill_process = require("bundle.kill_process")
-			local safari_tabs = require("bundle.safari_tabs")
-			local martillo = require("bundle.martillo")
-
-			return {
-				actions = {
-					window,
-					utilities,
-					converter,
-					screen,
-					network,
-					clipboard,
-					kill_process,
-					safari_tabs,
-					martillo,
-				},
-			}
-		end,
 		actions = {
 			{ "toggle_system_appearance", alias = "ta" },
-			{ "toggle_caffeinate", alias = "tc" },
-			{ "window_maximize", alias = "wm" },
-			{ "screen_ruler", alias = "ru" },
+			{ "toggle_caffeinate",        alias = "tc" },
+			{ "system_information",       alias = "si" },
+			{ "screen_ruler",             alias = "ru" },
 
-			{ "window_center", keys = { { "<leader>", "return" } } },
-			{ "window_almost_maximize", keys = { { "<leader>", "up" } } },
-			{ "window_reasonable_size", keys = { { "<leader>", "down" } } },
-			{ "window_left_two_thirds", keys = { { "<leader>", "left" } } },
-			{ "window_right_two_thirds", keys = { { "<leader>", "right" } } },
-			{ "window_left_third", alias = "wlt" },
-			{ "window_right_third", alias = "wrt" },
-			{ "window_center_third", alias = "wct" },
+			{ "window_maximize",          alias = "wm" },
+			{ "window_almost_maximize",   keys = { { "<leader>", "up" } } },
+			{ "window_reasonable_size",   keys = { { "<leader>", "down" } } },
+			{ "window_center",            keys = { { "<leader>", "return" } } },
+			{ "window_left_two_thirds",   keys = { { "<leader>", "left" } } },
+			{ "window_right_two_thirds",  keys = { { "<leader>", "right" } } },
 
-			{ "generate_uuid", alias = "gu" },
+			{ "clipboard_history",        keys = { { "<leader>", "-" } } },
+			{ "kill_process",             keys = { { "<leader>", "=" } } },
+			{ "safari_tabs",              keys = { { "alt", "tab" } } },
 
-			{ "converter_time", alias = "ct" },
-			{ "converter_colors", alias = "cc" },
-			{ "converter_base64", alias = "cb" },
-			{ "converter_jwt", alias = "cj" },
+			{ "generate_uuid",            alias = "gu" },
+			{ "word_count",               alias = "wc" },
 
-			{ "network_copy_ip", alias = "ni" },
-			{ "network_speed_test", alias = "ns" },
+			{ "converter_time",           alias = "ct" },
+			{ "converter_colors",         alias = "cc" },
+			{ "converter_base64",         alias = "cb" },
+			{ "converter_jwt",            alias = "cj" },
 
-			{ "clipboard_history", keys = { { "<leader>", "-" } } },
-			{ "kill_process", keys = { { "<leader>", "=" } } },
-			{ "safari_tabs", keys = { { "alt", "tab" } } },
+			{ "network_ip_geolocation",   alias = "ni" },
+			{ "network_speed_test",       alias = "ns" },
 
-			{ "screen_confetti", alias = "cf" },
+			{ "keyboard_lock",            alias = "kl" },
+			{ "keyboard_keep_alive",      alias = "ka" },
 
-			{ "martillo_reload", alias = "mr" },
-			{ "martillo_update", alias = "mu" },
+			{ "screen_confetti",          alias = "cf" },
+			{ "f1_standings",             alias = "f1" },
+			{ "idonthavespotify",         alias = "idhs" },
+
+			{ "martillo_reload",          alias = "mr" },
+			{ "martillo_update",          alias = "mu" },
 		},
 		keys = {
-			{ "<leader>", "space", desc = "Toggle Actions Launcher" },
+			{ "<leader>", "space" },
 		},
 	},
 
@@ -75,6 +53,7 @@ return require("martillo").setup({
 		keys = {
 			{ "<leader>", "c", app = "Calendar" },
 			{ "<leader>", "x", app = "Excalidraw" },
+			{ "<leader>", "f", app = "Finder" },
 			{ "<leader>", "i", app = "Figma" },
 			{ "<leader>", ";", app = "Ghostty" },
 			{ "<leader>", "u", app = "Google Meet" },
@@ -107,9 +86,9 @@ return require("martillo").setup({
 			default_app = "Safari",
 			redirect = {
 				{ match = { "*localhost*", "*127.0.0.1*", "*0.0.0.0*" }, app = "Helium" },
-				{ match = { "*autarc.energy*" }, app = "Helium" },
-				{ match = { "*fly.dev*" }, app = "Helium" },
-				{ match = { "*meet.google*" }, app = "Helium" },
+				{ match = { "*autarc.energy*" },                         app = "Helium" },
+				{ match = { "*fly.dev*" },                               app = "Helium" },
+				{ match = { "*meet.google*" },                           app = "Helium" },
 			},
 		},
 		config = function(spoon)
